45109a78473d9282e31f05923ce3edb2dedfb766
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2021-06-25 10:34:43 -0300

    Aula 05 Finalizada

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,9 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { Image } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
 import { theme } from '../../global/styles/theme';
-import { useAuth } from '../../hooks/auth';
 import { styles } from './styles';
 
 type Props = {
@@ -11,24 +9,16 @@ type Props = {
 }
 
 const Avatar: React.FC<Props> = ({ urlImage }) => {
-  const { signOut } = useAuth();
-
-  async function handleLogout() {
-    signOut()
-  }
-  
   return (
-    <RectButton onPress={handleLogout}>
-      <LinearGradient
-        style={styles.container}
-        colors={[theme.colors.secondary50, theme.colors.secondary70]}
-      >
-        <Image
-          source={{ uri: urlImage }}
-          style={styles.avatar}
-        />
-      </LinearGradient>
-    </RectButton>
+    <LinearGradient
+      style={styles.container}
+      colors={[theme.colors.secondary50, theme.colors.secondary70]}
+    >
+      <Image
+        source={{ uri: urlImage }}
+        style={styles.avatar}
+      />
+    </LinearGradient>
   );
 }
 
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import { useAuth } from '../../hooks/auth';
 import Avatar from '../Avatar';
 import { styles } from './styles';
 
+const textos = [
+  "Hoje é dia de conquista",
+  "Hoje a vitória será sua!",
+  "Hoje GG não escapa",
+  "Hoje é dia de ganhar"
+]
+
+const randomIndex = Math.floor(Math.random() * 4)
+
 const Profile: React.FC = () => {
   const { user } = useAuth()
 
-  const textos = [
-    "Hoje é dia de conquista",
-    "Hoje a vitória será sua!",
-    "Hoje GG não escapa",
-    "Hoje é dia de ganhar"
-  ]
+  const { signOut } = useAuth();
 
-  const randomIndex = Math.floor(Math.random() * 4)
+  async function handleLogout() {
+    Alert.alert('Sair', 'Deseja sair do ggplay?', [
+      {
+        text: 'Não',
+        style: 'cancel',
+      },
+      {
+        text: 'Sim',
+        onPress: signOut,
+      },
+    ])
+  }
 
   return (
     <View style={styles.container}>
-      <Avatar urlImage={user.avatar} />
+      <RectButton onPress={handleLogout}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
 
       <View>
         <View style={styles.user}>
